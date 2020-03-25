0227790d2b08107402c2c8a6cd3c8d4a39504dc6
Flávio Lehmann Júnior <flaviojunior0899@gmail.com>
2020-03-25 19:31:51 -0300

    rm: hello world code at routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,14 +2,6 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.post('/teste', (req, res) => {
-    console.log(req.body);
-})
 
-routes.get('/', (req, res) => {
-    return res.json({
-        msg: 'Hello World! 2'
-    });
-})
 
 module.exports = routes;
