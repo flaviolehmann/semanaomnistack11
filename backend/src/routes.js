3b85874968ff445c950e261461dba58412f7cd38
Flávio Lehmann Júnior <flaviojunior0899@gmail.com>
2020-03-25 20:07:41 -0300

    feat: create [POST] /ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
+routes.post('/ongs', async (req, res) => {
+    const newOng = {
+        ...req.body,
+        id: crypto.randomBytes(4).toString('HEX')
+    }
 
+    await connection('ongs').insert(newOng);
+    return res.json({ id: newOng.id });
+})
 
 module.exports = routes;
